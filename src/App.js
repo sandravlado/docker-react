cdcba6216f9ab86dfe1ec2bf05e016f68ce9ada7
root <root@LAPTOP-HDPO9MGU.localdomain>
2022-07-24 09:55:30 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from Node.js
+          We are learning Docker.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          className="App-intro">
+          I was changed!
+          Learn React!
+          Another one bites the dust!
+        </a> 
       </header>
     </div>
   );
